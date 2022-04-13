44d27d21c40332737c4c67c6914649fb805276d6
Natalia Alferova <natalia@alferova.me>
2022-04-13 10:09:01 +0200

    Deleted //TODO

--- src/test/java/ru/netology/AuthTest.java
+++ src/test/java/ru/netology/AuthTest.java
@@ -12,21 +12,16 @@ import static ru.netology.DataGenerator.Registration.*;
 import static ru.netology.DataGenerator.getRandomLogin;
 import static ru.netology.DataGenerator.getRandomPassword;
 
-// спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
+
 public class AuthTest {
 
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
-//        Configuration.browser = "chrome";
-//        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void shouldSuccessfulLoginIfRegisteredActiveUser() {
-        // TODO: добавить логику теста, в рамках которого будет выполнена попытка входа в личный кабинет с учётными
-        //  данными зарегистрированного активного пользователя, для заполнения полей формы используйте
-        //  пользователя registeredUser
         DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
         DataGenerator.sendRequest(registeredUser);
         $("[name='login']").setValue(registeredUser.getLogin());
@@ -37,54 +32,44 @@ public class AuthTest {
 
     @Test
     void shouldGetErrorIfNotRegisteredUser() {
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
-        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
         DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
         DataGenerator.sendRequest(registeredUser);
         DataGenerator.RegistrationDto notRegisteredUser = getUser("active");
         $("[name='login']").setValue(notRegisteredUser.getLogin());
         $("[name='password']").setValue(notRegisteredUser.getPassword());
         $(withText("Продолжить")).click();
-        $("[class='notification__title']").shouldHave(Condition.text("Ошибка"));
+        $("[class='notification__content']").shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
 
     @Test
     void shouldGetErrorIfBlockedUser() {
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
-        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
         DataGenerator.RegistrationDto blockedUser = getRegisteredUser("blocked");
         DataGenerator.sendRequest(blockedUser);
         $("[name='login']").setValue(blockedUser.getLogin());
         $("[name='password']").setValue(blockedUser.getPassword());
         $(withText("Продолжить")).click();
-        $("[class='notification__title']").shouldHave(Condition.text("Ошибка"));
+        $("[class='notification__content']").shouldHave(Condition.text("Пользователь заблокирован"));
     }
 
     @Test
     void shouldGetErrorIfWrongLogin() {
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
-        //  "Пароль" - пользователя registeredUser
         DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
         DataGenerator.sendRequest(registeredUser);
         String wrongLogin = getRandomLogin();
         $("[name='login']").setValue(wrongLogin);
         $("[name='password']").setValue(registeredUser.getPassword());
         $(withText("Продолжить")).click();
-        $("[class='notification__title']").shouldHave(Condition.text("Ошибка"));
+        $("[class='notification__content']").shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
 
     @Test
     void shouldGetErrorIfWrongPassword() {
-        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
-        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
-        //  "Пароль" - переменную wrongPassword
         DataGenerator.RegistrationDto registeredUser = getRegisteredUser("active");
         DataGenerator.sendRequest(registeredUser);
         String wrongPassword = getRandomPassword();
         $("[name='login']").setValue(registeredUser.getLogin());
         $("[name='password']").setValue(wrongPassword);
         $(withText("Продолжить")).click();
-        $("[class='notification__title']").shouldHave(Condition.text("Ошибка"));
+        $("[class='notification__content']").shouldHave(Condition.text("Неверно указан логин или пароль"));
     }
 }
